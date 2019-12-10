39593fe8518813f80661867147d5c8c4f556d543
Sepideh Miralaeigit config --global user.email sepideh.miralaei@shopify.comgit config --global user.namegit config --global user.name Sepideh <sepideh.miralaei@shopify.com>
2019-12-10 16:49:50 -0500

    fixed repeated wishlist items

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -5,7 +5,8 @@ class Wishlist < ApplicationRecord
   validates :notes, length: {maximum: 500,
                              too_long: "500 characters is the maximum allowed"},
                     allow_blank: true
-#  validates :name, presence: true, uniqueness: true
+# validates :name, presence: true, uniqueness: true
+ validates :name, uniqueness: { scope: :user_id }
   
 end
 
